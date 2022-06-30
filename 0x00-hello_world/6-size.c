bc57761f01b90ef9fd67ac743e567133b432a9ae
damfery <adesinadamilola04@gmail.com>
2022-06-30 13:13:46 +0000

    trust and obey

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,15 @@ int main(void)
 {
 	char charType;
 	int intType;
-	long longType;
 	long int longintType;
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of int: %lu bytes\n", sizeof(charType));
-	printf("Size of float: %lu bytes\n", sizeof(intType));
-	printf("Size of long: %lu bytes\n", sizeof(longintType));
-	printf("Size of long long int: %lu bytes\n", sizeof(longlongintType));
-	printf("Size of float: floatType: %lu bytes \n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %lu byte(s) \n", sizeof(floatType));
 
 	return (0);
 }

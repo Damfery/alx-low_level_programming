5b5032f23028f67b26c0f2f2d15b31d7d4f4bb5f
damfery <adesinadamilola04@gmail.com>
2022-07-01 16:55:16 +0000

    ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - first ten numbers in base 10
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+
+	printf("%d", var);
+
+	printf("\n");
+
+	return (0);
+}

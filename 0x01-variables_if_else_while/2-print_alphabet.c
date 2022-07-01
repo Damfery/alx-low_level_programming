8dd640f8a735602665f43e0a04371fc6e7271425
damfery <adesinadamilola04@gmail.com>
2022-07-01 15:53:12 +0000

    topic 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{ putchar(alp); }
+
+	putchar('\n');
+	return (0);
+
+}
+

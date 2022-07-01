62811bfe56457f65405f9979d09858e359c5fc0f
damfery <adesinadamilola04@gmail.com>
2022-07-01 16:43:40 +0000

    apart from e and p

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print alphabeth apart from q and e
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+
+	{
+		if (al == 'e' || al == 'q')
+		continue;
+	}
+		{ putchar(al); }
+
+	putchar('\n');
+	return (0);
+}

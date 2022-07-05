c83df50767c1a6a268dc7a9f1519f8e7de2da1bd
damfery <adesinadamilola04@gmail.com>
2022-07-05 15:28:46 +0000

    task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - this prints all lower alphabet
+ * 'c' - prints int
+ * Return: 1 if c is lowercase, 0 (otherwise)
+ */
+
+int _islower(int c)
+
+{
+	c = _islower('c');
+	_putchar(c + '0');
+	return (0);
+}

036db1638395ea584a279d55e7669ba903f464b3
damfery <adesinadamilola04@gmail.com>
2022-07-22 12:02:19 +0000

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- what it is
+ * Return: 0 on success
+ * @argc: argument count
+ * @argv: argurment vector
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

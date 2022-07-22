8529a6852d2dff53bfb0d641767093161c24659c
damfery <adesinadamilola04@gmail.com>
2022-07-22 12:29:04 +0000

    prints arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- what it is
+ * Return: 0 on success
+ * @argc: argument count
+ * @argv: argurment vector
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc -1);
+	return (0);
+}

f611f0c8b5aad5993b443a75c0ee2e063d31b16f
damfery <adesinadamilola04@gmail.com>
2022-08-01 11:38:08 +0000

    dog correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG_H
 #define DOG_H
 
 /*
@@ -18,3 +19,5 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif

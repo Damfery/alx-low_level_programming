73101a822367a48920266116c0207645fa8e5b05
damfery <adesinadamilola04@gmail.com>
2022-08-16 10:16:14 +0000

    putchar

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,20 +4,20 @@
 
 /**
  * print_listint - print all elements of a listint_t list
- * @h: pointer to the first element of the list
+ * @h: integer
  *
  * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t var = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		var++;
 	}
-	return (i);
+	return (var);
 }
